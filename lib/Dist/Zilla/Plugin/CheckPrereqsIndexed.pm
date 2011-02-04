46c4d0cca408b4e253b8d9265c8a9cbb70c31b3c
Ricardo Signes <rjbs@cpan.org>
2011-02-04 17:52:31 -0500

    real tests, talk to cpanmetadb for now

--- lib/Dist/Zilla/Plugin/CheckPrereqsIndexed.pm
+++ lib/Dist/Zilla/Plugin/CheckPrereqsIndexed.pm
@@ -4,6 +4,7 @@ use Moose;
 with 'Dist::Zilla::Role::BeforeRelease';
 
 use List::MoreUtils qw(uniq);
+use LWP::UserAgent;
 
 use namespace::autoclean;
 
@@ -17,12 +18,31 @@ sub before_release {
                  map {; values %$_ }
                  values %$prereqs;
 
-  # get names of all packages in any kind of requirement
-  # look up each requirement in 02packages or cpandb or something
-  # make a list of requirements that aren't found
-  # if the list is empty, return
-  # otherwise, prompt: abort or continue
-  $self->log("@packages");
+  return unless @packages; # no prereqs!?
+
+  my $ua = LWP::UserAgent->new(keep_alive => 1);
+
+  my %missing;
+
+  for my $pkg (@packages) {
+    my $res = $ua->get("http://cpanmetadb.appspot.com/v1.0/package/$pkg");
+    $missing{ $pkg } = 1 unless $res->is_success;
+  }
+
+  unless (keys %missing) {
+    $self->log("all prereqs appear to be indexed");
+    return;
+  }
+
+  my @missing = sort keys %missing;
+
+  $self->log("the following prereqs could not be found on CPAN: @missing");
+  return if $self->zilla->chrome->prompt_yn(
+    "release despite missing prereqs?",
+    { default => 0 }
+  );
+
+  $self->log_fatal("aborting release due to apparently unindexed prereqs");
 }
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -8,8 +8,9 @@ use lib 't/lib';
 use Test::DZil;
 
 sub new_tzil {
+  my ($corpus_dir) = @_;
   my $tzil = Builder->from_config(
-    { dist_root => 'corpus/DZT' },
+    { dist_root => $corpus_dir },
     {
       add_files => {
         'source/dist.ini' => simple_ini(
@@ -21,11 +22,23 @@ sub new_tzil {
 }
 
 {
-  my $tzil = new_tzil;
+  my $tzil = new_tzil('corpus/DZT');
 
-  $tzil->release;
+  my $err = exception { $tzil->release };
 
-  diag $_ for @{ $tzil->log_messages };
+  like($err, qr/unindexed prereq/, "we aborted because we had weird prereqs");
+  ok(
+    (grep { /Zorch/ } @{ $tzil->log_messages }),
+    "and we specifically mentioned the one we expected",
+  );
+}
+
+{
+  my $tzil = new_tzil('corpus/DZZ');
+
+  my $err = exception { $tzil->release };
+
+  is($err, undef, "we released with no errors");
 }
 
 done_testing;
