68bb2d0d2829247beb404d7f4dce2c1196da0da0
Ricardo Signes <rjbs@cpan.org>
2011-08-18 11:04:36 -0400

    require that CPAN have a high-enough version, too

--- lib/Dist/Zilla/Plugin/CheckPrereqsIndexed.pm
+++ lib/Dist/Zilla/Plugin/CheckPrereqsIndexed.pm
@@ -2,6 +2,8 @@ package Dist::Zilla::Plugin::CheckPrereqsIndexed;
 use Moose;
 # ABSTRACT: prevent a release if you have prereqs not found on CPAN
 
+use 5.10.0; # //
+
 =head1 OVERVIEW
 
 Sometimes, AutoPrereqs is a little overzealous and finds a prereq that you
@@ -20,42 +22,89 @@ releasing while offline impossible... but it was anyway, right?
 
 with 'Dist::Zilla::Role::BeforeRelease';
 
+use Encode qw(encode_utf8);
 use List::MoreUtils qw(uniq);
 use LWP::UserAgent;
+use version ();
+use YAML::XS qw(Load);
 
 use namespace::autoclean;
 
 sub before_release {
   my ($self) = @_;
 
-  my $prereqs  = $self->zilla->prereqs->as_string_hash;
+  my $prereqs_hash  = $self->zilla->prereqs->as_string_hash;
 
-  my @packages = sort { $a cmp $b } uniq
-                 grep { $_ ne 'Config' } # special case -- rjbs, 2011-05-20
-                 grep { $_ ne 'perl' } # special case -- rjbs, 2011-02-05
-                 map  {; keys %$_ }
-                 map  {; values %$_ }
-                 values %$prereqs;
+  my %requirement;
 
-  return unless @packages; # no prereqs!?
+  # first level keys are phase; second level keys are types; we will just merge
+  # everything -- rjbs, 2011-08-18
+  for my $req_set (map { values %$_ } values %$prereqs_hash) {
+    REQ_PKG: for my $pkg (keys %$req_set) {
+      next if $pkg eq 'Config'; # special case -- rjbs, 2011-05-20
+      next if $pkg eq 'perl';   # special case -- rjbs, 2011-02-05
+
+      my $ver   = $req_set->{$pkg};
+
+      $requirement{ $pkg } //= version->parse(0);
+
+      # we have a complex, stupid rule -- rjbs, 2011-08-18
+      next REQ_PKG if $ver =~ /<>=,\s/;
+
+      $requirement{ $pkg } = $ver
+        if version->parse($ver) > $requirement{ $pkg };
+    }
+  }
+
+  return unless keys %requirement; # no prereqs!?
 
   my $ua = LWP::UserAgent->new(keep_alive => 1);
 
   my %missing;
+  my %too_new;
 
-  for my $pkg (@packages) {
+  PKG: for my $pkg (sort keys %requirement) {
     my $res = $ua->get("http://cpanmetadb.appspot.com/v1.0/package/$pkg");
-    $missing{ $pkg } = 1 unless $res->is_success;
+    unless ($res->is_success) {
+      $missing{ $pkg } = 1;
+      next PKG;
+    }
+
+    # YAML::XS only works with UTF-8 bytestreams, so we need to re-encode no
+    # matter what encoding we got. -- rjbs, 2011-08-18
+    my $yaml_octets = encode_utf8($res->decoded_content);
+    my $payload = Load($yaml_octets);
+
+    my $indexed_version = version->parse($payload->{version});
+    next PKG if $indexed_version >= $requirement{ $pkg };
+
+    $too_new{ $pkg } = {
+      required => $requirement{ $pkg },
+      indexed  => $indexed_version,
+    };
   }
 
-  unless (keys %missing) {
+  unless (keys %missing or keys %too_new) {
     $self->log("all prereqs appear to be indexed");
     return;
   }
 
-  my @missing = sort keys %missing;
+  if (keys %missing) {
+    my @missing = sort keys %missing;
+    $self->log("the following prereqs could not be found on CPAN: @missing");
+  }
 
-  $self->log("the following prereqs could not be found on CPAN: @missing");
+  if (keys %too_new) {
+    for my $pkg (sort keys %too_new) {
+      $self->log([
+        "you required %s version %s but CPAN only has version %s",
+        $pkg,
+        "$too_new{$pkg}{required}",
+        "$too_new{$pkg}{indexed}",
+      ]);
+    }
+  }
+
   return if $self->zilla->chrome->prompt_yn(
     "release despite missing prereqs?",
     { default => 0 }
--- t/basic.t
+++ t/basic.t
@@ -34,6 +34,31 @@ sub new_tzil {
 }
 
 {
+  my $tzil = Builder->from_config(
+    { dist_root => 'corpus/DZZ' },
+    {
+      add_files => {
+        'source/dist.ini' => simple_ini(
+          qw(GatherDir AutoPrereqs CheckPrereqsIndexed FakeRelease),
+          [ Prereqs => { 'Dist::Zilla' => 99 } ],
+        ),
+      },
+    },
+  );
+
+  my $err = exception { $tzil->release };
+
+  like($err, qr/unindexed prereq/, "we aborted because we had weird prereqs");
+
+  ok(
+    (grep { /you required Dist::Zilla version 99/ } @{ $tzil->log_messages }),
+    "it complained that we wanted a too-new version",
+  );
+}
+
+{
+  # This is to test that we don't have any problems with libraries that are in
+  # our own dist.
   my $tzil = new_tzil('corpus/DZZ');
 
   my $err = exception { $tzil->release };
